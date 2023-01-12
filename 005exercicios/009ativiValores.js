747d85e557f834c782b3a70b29ee4efb9567ef80
G-SSC <78927301+G-SSC@users.noreply.github.com>
2023-01-12 17:41:34 -0300

    up

---
+++ 005exercicios/009ativiValores.js
@@ -0,0 +1,10 @@
+
+// O valor :(=)  Valor igual:(==) Identico:(===) Soma:(+) Subtração:(-) Multiplicação (*) Divisão(/) Potencia = (**) Sobra da dívisão(%)
+vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 80) {
+    console.log(`Você ultrapassou a velocidade`)
+    }else
+    {
+    console.log(`Boa viajem`)
+    }
