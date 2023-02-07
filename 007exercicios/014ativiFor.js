08bd473a6d10c6b7c0554a98b449ba48b7cef00b
G-SSC <78927301+G-SSC@users.noreply.github.com>
2023-02-07 10:36:18 -0300

    up

---
+++ 007exercicios/014ativiFor.js
@@ -0,0 +1,5 @@
+for(c = 1; c <= 4; c++)
+    {
+    console.log(c)    
+    }
+console.log('Fim')
---
+++ 008exercicios/ativiArray.js
@@ -0,0 +1,3 @@
+let num = [5, 3, 4, 5, 6];
+
+console.log(`O arrey de ${num.length} valores, contem os valores ${num}, para adicionar um novo valor, é apenas digitar "num.push(), e para colocar os valores do array em ordem crescente, é apenas escrever "num.sort"`)
