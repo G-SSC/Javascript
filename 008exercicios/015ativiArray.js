5b89c06fcb84e39fc63734682862e043375a00c2
G-SSC <78927301+G-SSC@users.noreply.github.com>
2023-02-08 17:02:35 -0300

    up

--- 008exercicios/015ativiArray.js
+++ 008exercicios/015ativiArray.js
@@ -1,3 +1,9 @@
 let num = [5, 3, 4, 5, 6];
 
 console.log(`O arrey de ${num.length} valores, contem os valores ${num}, para adicionar um novo valor, é apenas digitar "num.push(), e para colocar os valores do array em ordem crescente, é apenas escrever "num.sort"`)
+
+for(let pos in num)
+    {
+    console.log(`A posição ${pos}`)
+    }
+    console.log(num.indexOf(2), num.indexOf(3))
---
+++ 009exercicios/016Functions.js
@@ -0,0 +1,12 @@
+valor = function parimpar(n)
+    {
+    if(n%2 == 0)
+        {
+        return 'Par'
+        }
+        else
+            {
+            return 'Impar'
+            }
+    }
+console.log(valor(5));
