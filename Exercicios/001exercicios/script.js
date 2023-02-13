c8c7f1a3f4e8434a047d309ffb17ce259f5d28bc
G-SSC <78927301+G-SSC@users.noreply.github.com>
2023-02-13 00:29:17 -0300

    up

---
+++ Exercicios/001exercicios/script.js
@@ -0,0 +1,19 @@
+
+function carregar()
+    {
+    msg = window.document.getElementById('msg');
+    img = window.document.getElementById('img');
+    agora = new Date();
+    time = 20;
+    msg.innerHTML = `Agora são ${time} horas.`;
+    if(time>=6 && time<12)
+        {
+        img.src = 'imagens/dia.jpg'
+        document.body.style.background ='rgba(176, 213, 255, 0.473)'
+        } 
+        else if(time>=12 && time<18)
+            {
+            img.src = 'imagens/tarde.jpg'
+            document.body.style.background ='rgb(206, 104, 9)'
+            } 
+    }
