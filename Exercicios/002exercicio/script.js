e311d5d647e38b50d62d34a10c0004bf4f6842bb
G-SSC <78927301+G-SSC@users.noreply.github.com>
2023-02-14 11:15:14 -0300

    up

---
+++ Exercicios/002exercicio/script.js
@@ -0,0 +1,8 @@
+function verificar()
+    {
+    data = new Date()
+    ano = data.getFullYear()
+    verfano = document.getElementById("txtidade")
+    res = document.querySelector('div#resp')
+    if 
+    }
